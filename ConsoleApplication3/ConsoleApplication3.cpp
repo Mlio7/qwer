ad21b123db3fc36de2f3d6f5a35b5ef9d99481fd
Vladyslav Tymoshchuk <Vladyslav.Tymoshchuk@natec.ua>
2021-07-21 18:38:32 +0300

    complete

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+struct ShowParam 
+{
+	int startLine;
+	int endLine;
+};
+
+bool start();
+string comunicate();
+vector<string> read(string);
+ShowParam getShowParam(const vector<string>&);
+void show(const vector<string>&, const ShowParam&);
+void endScreen();
+
 int main()
 {
-	for (size_t i =	1; i != 6; i++)
+	
+	if (start()) 
 	{
-		cout << "Hello World!!!\n";
+		auto fileName = comunicate();
+		auto content = read(fileName);
+		auto sp = getShowParam(content);
+		show(content, sp);
 	}
+
+	endScreen();
+
     return 0;
 }
